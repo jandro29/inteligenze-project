e41f7a4846618dcfb68a5d7543986d311cbadcf3
jandro29 <ascolanr@gmail.com>
2025-09-23 16:44:00 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,17 @@ const nextConfig = {
  output: 'export',
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
+  eslint: { ignoreDuringBuilds: true },
+
+
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+
+  experimental: {
+    dynamic: 'force-dynamic'
+  }
+
 };
 
 export default nextConfig;
